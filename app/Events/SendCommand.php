370ec11189a326dc641ecfa7036bfb71fca0c608
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-21 03:08:17 +0000

    events

--- app/Events/SendCommand.php
+++ app/Events/SendCommand.php
@@ -9,7 +9,7 @@ use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class SendCommand
+class SendCommand implements ShouldBroadcast
 {
     use InteractsWithSockets, SerializesModels;
     public $command;
