3a64f305faa915aeb1292d870059425f9df72c20
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-21 02:14:47 -0500

    download file

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AudioController extends Controller
 {
@@ -10,5 +11,10 @@ class AudioController extends Controller
     public function upload(Request $request){
       $fn = str_random(25);
       $path = $request->audio->storeAs('audio', "{$fn}.3gp");
+    }
+
+    public function download($filename){
+      $url = Storage::url('file1.jpg');
+      return response()->file($url);
     }
 }
