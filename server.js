425503c505799bf3f9afa2b3fbb5954a0323b29f
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-21 01:58:26 +0000

    Init Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+
+var Redis = require('ioredis');
+var redis = new Redis();
+
+app.listen(6001, function() {
+    console.log('Server is running!');
+});
+
+function handler(req, res) {
+    res.writeHead(200);
+    res.end('Test');
+}
+
+io.on('connection', function(socket) {
+    //
+    console.log('Connected');
+    io.emit("event","camera");
+    io.emit("event","toast");
+
+});
+io.on('disconnect', function(socket) {
+    //
+    console.log('Disconnected');
+
+});
+
+redis.psubscribe('*', function(err, count) {
+    //
+});
+
+redis.on('pmessage', function(subscribed, channel, message) {
+    message = JSON.parse(message);
+    io.emit(channel + ':' + message.event, message.data);
+});
