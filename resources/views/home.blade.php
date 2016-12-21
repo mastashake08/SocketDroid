783e627e4d9bb60e034d151d884c4f68c11290e4
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-21 09:34:50 -0500

    jquery

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,8 @@
 
 @section('content')
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script>
-function stopAudio(){
-  var xmlHttp = new XMLHttpRequest();
-   xmlHttp.open( "GET", http://socket.jyroneparker.com/command/audio-stop, false ); // false for synchronous request
-   xmlHttp.send( null );
-}
-function startAudio(){
-  var xmlHttp = new XMLHttpRequest();
-   xmlHttp.open( "GET", http://socket.jyroneparker.com/command/audio-start, false ); // false for synchronous request
-   xmlHttp.send( null );
-}
-</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -21,12 +11,25 @@ function startAudio(){
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    <button class="btn btn-sm btn-default" id="start-audio" onclick="startAudio()"><i class="fa fa-microphone" aria-hidden="true"></i></button>
-                    <button class="btn btn-sm btn-default" id="stop-audio" onclick="stopAudio()"><i class="fa fa-microphone-slash" aria-hidden="true"></i></button>
+                    <button class="btn btn-sm btn-default" id="start-audio" ><i class="fa fa-microphone" aria-hidden="true"></i></button>
+                    <button class="btn btn-sm btn-default" id="stop-audio"><i class="fa fa-microphone-slash" aria-hidden="true"></i></button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+$('#start-audio').click(function(){
+
+
+});
+$('#stop-audio').click(function(){
+  $.get("http://socket.jyroneparker.com/command/audio-start", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+
+});
+
+</script>
 
 @endsection
