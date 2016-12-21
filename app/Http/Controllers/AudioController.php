acbc9cd4e7c257203835af0ad2925eed0d87cd74
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-21 02:34:54 -0500

    notifications

--- app/Http/Controllers/AudioController.php
+++ app/Http/Controllers/AudioController.php
@@ -11,10 +11,15 @@ class AudioController extends Controller
     public function upload(Request $request){
       $fn = str_random(25);
       $path = $request->audio->storeAs('audio', "{$fn}.3gp");
+      $user = User::all();
+      $user->each(function($item, $key){
+        $item->notify(new \App\Notifications\FileUploaded($fn));
+      });
     }
 
     public function download($filename){
       $url = storage_path('app/audio/'.$filename);
+
       return response()->file($url);
     }
 }
---
+++ app/Notifications/FileUploaded.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class FileUploaded extends Notification
+{
+    use Queueable;
+    public $fn;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($fn)
+    {
+        //
+        $this->fn = $fn;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Audio Ready For Download')
+                    ->line('Your audio is ready for download')
+                    ->action('Download', "http://socket.jyroneparker.com/download/{$fn}")
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
