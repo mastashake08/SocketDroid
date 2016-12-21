9e0520e8d20b2665042b8b31af6a21916df547ec
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-21 09:26:07 -0500

    front end start

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,23 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <button class="btn btn-sm btn-default" id="start-audio" onclick="startAudio()"><i class="fa fa-microphone" aria-hidden="true"></i></button>
+                    <button class="btn btn-sm btn-default" id="stop-audio" onclick="stopAudio()"><i class="fa fa-microphone-slash" aria-hidden="true"></i></button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+function stopAudio(){
+  var xmlHttp = new XMLHttpRequest();
+   xmlHttp.open( "GET", http://socket.jyroneparker.com/command/audio-stop, false ); // false for synchronous request
+   xmlHttp.send( null );
+}
+function startAudio(){
+  var xmlHttp = new XMLHttpRequest();
+   xmlHttp.open( "GET", http://socket.jyroneparker.com/command/audio-start, false ); // false for synchronous request
+   xmlHttp.send( null );
+}
+</script>
 @endsection
