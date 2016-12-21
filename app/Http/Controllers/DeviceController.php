dbf75c3abc69cf3012b8fe9dca946bf080aa83f7
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-21 18:42:37 -0500

    image

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -16,9 +16,9 @@ class DeviceController extends Controller
     }
 
     public function uploadImage(Request $request){
-      \Log::info($request->picture);
+      \Log::info($request->image);
       $fn = str_random(25);
-      $path = $request->picture->storeAs('images', "{$fn}.png");
+      $path = $request->image>storeAs('images', "{$fn}.png");
       $user = \App\User::all();
       $user->each(function($item, $key) use($fn){
         $item->notify(new \App\Notifications\ImageUploaded($fn));
