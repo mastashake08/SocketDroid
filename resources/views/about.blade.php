553840134566fc5462184d56f1580437446228d4
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-22 09:13:12 -0500

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,7 +9,10 @@
                 <div class="panel-heading">How Does It Work?</div>
 
                 <div class="panel-body">
-                  
+                  SocketDroid is a SAAS application that allows you to remotely
+                  monitor and control android phones that have the companion app
+                  installed. Simply make a free account, add the phone number of
+                  the device(s) you want to control and start sending commands.
                 </div>
             </div>
         </div>
