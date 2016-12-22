3b375c26d78ff999b6d1b3e1d326fe622ca9a508
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-21 23:21:03 -0500

    about page

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::post('/post-gps','GpsController@postGps');
 Route::resource('gps','GpsController');
 Route::resource('device','DeviceController');
 Route::post('/post-battery', 'BatteryController@getBattery');
+Route::get('/about',function(){
+  return view('about');
+});
