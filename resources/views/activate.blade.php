d6d485cd2148a8c135775595ad7f9013ecad7b24
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-22 16:31:37 -0500

    code

--- resources/views/activate.blade.php
+++ resources/views/activate.blade.php
@@ -16,7 +16,7 @@
                           <label for="name" class="col-md-4 control-label">Name</label>
 
                           <div class="col-md-6">
-                              <input id="name" type="text" class="form-control" name="activate" value="{{ old('activate') }}" required autofocus>
+                              <input id="name" type="text" class="form-control" name="code" value="{{ old('code') }}" required autofocus>
 
                               @if ($errors->has('name'))
                                   <span class="help-block">
