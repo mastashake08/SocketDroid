3d2cbc3e27d0388db2ac6932ee1a446e386cc0ce
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-22 19:22:00 -0500

    play badge

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,6 +86,7 @@
                     <a href="/how-it-works">How It Works</a>
                     <a href="https://jyroneparker.com">Meet The Developer</a>
                 </div>
+                <a href='https://play.google.com/store/apps/details?id=com.socketdroid.mastashake08.socketdroid&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
             </div>
         </div>
     </body>
