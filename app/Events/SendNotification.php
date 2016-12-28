6ab390bb326e16294c6caaf83ca67b1bb3cfe4cf
Jyrone Parker <jyrone.parker@gmail.com>
2016-12-28 18:12:16 -0500

    broadcast

--- app/Events/SendNotification.php
+++ app/Events/SendNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class SendNotification
+class SendNotification implements ShouldBroadcast
 {
     use InteractsWithSockets, SerializesModels;
     public $title, $message;
